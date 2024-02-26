3ddcefd8583117d49d940ab728aa66c169670ac9
Morgan <svj4th@virginia.edu>
2024-02-26 18:06:13 -0500

    Added support for 'site admins', app properly distinguishes normal users from site admins, viewable in Django admin site

--- b26/admin.py
+++ b26/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import UserProfile
+
 # Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'is_site_admin']
+    list_filter = ['is_site_admin']
+    search_fields = ['user__username']
