1a15d960082b33aaf4eb3c227648f1000e085204
Morgan <svj4th@virginia.edu>
2024-03-15 18:06:21 -0400

    Fixed formatting in b26 subdirectory

--- b26/models.py
+++ b26/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from django.conf import settings
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
