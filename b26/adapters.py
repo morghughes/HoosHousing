485e7d26dc423125843133366a0f101fe97856a3
Morgan Hughes <143117544+morghughes@users.noreply.github.com>
2024-04-15 00:31:48 -0400

    Added optional first/last names to manual sign-in page

--- b26/adapters.py
+++ b26/adapters.py
@@ -3,8 +3,11 @@ from django.utils.text import slugify
 
 class CustomAccountAdapter(DefaultAccountAdapter):
     def populate_username(self, request, user):
-        username_base = user.email.split('@')[0]
-        username = slugify(username_base)  # Makes username all lowercase and replaces spaces with hyphens
+        if user.email:
+            username_base = user.email.split('@')[0]
+            username = slugify(username_base)  # Makes username all lowercase and replaces spaces with hyphens
+        else:
+            username = request.POST.get('username', '')
         count = 0
 
         while True:
@@ -22,3 +25,14 @@ class CustomAccountAdapter(DefaultAccountAdapter):
     def username_exists(self, username):
         from allauth.account.models import EmailAddress
         return EmailAddress.objects.filter(user__username=username).exists()
+
+    def save_user(self, request, user, form, commit=True):  # Saves users that sign in manually, with the custom, optional first and last name fields
+        user = super().save_user(request, user, form, commit=False)
+
+        # Now handle the additional fields
+        user.first_name = request.POST.get('first_name', '')
+        user.last_name = request.POST.get('last_name', '')
+
+        if commit:
+            user.save()
+        return user
