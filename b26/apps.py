aa9b45c8bdfcdb475ff9c02bfdb549f1b07074a3
Morgan Hughes <143117544+morghughes@users.noreply.github.com>
2024-04-17 13:48:50 -0400

    Changed user flow (urls), slight button changes on some pages

--- b26/apps.py
+++ b26/apps.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 class B26Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'b26'
-
-    def ready(self):
-        import b26.signals
--- b26/signals.py
+++
@@ -1,6 +0,0 @@
-from django.dispatch import receiver
-from allauth.account.signals import user_logged_in
-
-@receiver(user_logged_in)
-def set_just_logged_in_flag(sender, request, user, **kwargs):
-    request.session['just_logged_in'] = True
