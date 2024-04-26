f6495cb4ffbb27e1e438d1cf9dbbf13cf92f3c94
Morgan Hughes <143117544+morghughes@users.noreply.github.com>
2024-04-26 17:56:10 -0400

    Fixed bug where unintended behavior occurred when user attempts to use Google login with same email that they manually signed up with

--- b26/adapters.py
+++ b26/adapters.py
@@ -1,5 +1,8 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from django.utils.text import slugify
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.account.utils import perform_login
+from django.shortcuts import redirect
 
 class CustomAccountAdapter(DefaultAccountAdapter):
     def populate_username(self, request, user):
@@ -26,7 +29,8 @@ class CustomAccountAdapter(DefaultAccountAdapter):
         from allauth.account.models import EmailAddress
         return EmailAddress.objects.filter(user__username=username).exists()
 
-    def save_user(self, request, user, form, commit=True):  # Saves users that sign in manually, with the custom, optional first and last name fields
+    def save_user(self, request, user, form, commit=True):  # Saves users that sign in manually, with the custom,
+        # optional first and last name fields
         user = super().save_user(request, user, form, commit=False)
 
         # Now handle the additional fields
@@ -36,3 +40,32 @@ class CustomAccountAdapter(DefaultAccountAdapter):
         if commit:
             user.save()
         return user
+
+
+class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
+    def pre_social_login(self, request, sociallogin):
+        from allauth.account.models import EmailAddress
+
+        if sociallogin.is_existing:  # if a matching social account already exists, just log the user into that
+            return
+        if 'email' in sociallogin.account.extra_data:  # if there is an email connected to  the current attempted
+            # social login (should always be), then extract it, all lowercase
+            email = sociallogin.account.extra_data['email'].lower()
+
+            try:
+                email_address = EmailAddress.objects.get(email=email)  # search list of emails, in database, entered
+                # through both manual and Google signups, find email that matches current attempted social login
+                user = email_address.user  # find user associated with email in database
+
+                if not email_address.verified:  # if email is not already verified (i.e. manual signup attempting
+                    # Google login for first time), then verify the email
+                    email_address.verified = True
+                    email_address.save()
+
+                sociallogin.connect(request, user)  # connect new sociallogin/account to existing user with matching
+                # email and login -- existing user now has a social account
+                return perform_login(request, user, 'none')
+
+            except EmailAddress.DoesNotExist:  # if no email matches sociallogin attempt email, then just go on
+                # creating a branch new user and social account
+                pass
